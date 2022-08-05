14c29c44910782b3f312d9fb3da71bb98237106a
subpurple <102702106+subpurple@users.noreply.github.com>
2022-08-05 15:40:23 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,4 +1,10 @@
 <center>
-            <?php if (empty($_SERVER['HTTPS'])){echo '<a href="https://' . $_SERVER[HTTP_HOST] . $_SERVER[REQUEST_URI] . '">Switch to HTTPS</a>';}else{echo '<a href="http://' . $_SERVER[HTTP_HOST] . $_SERVER[REQUEST_URI] . '">Switch to HTTP</a>';} ?>
-            <address>&copy; <?php echo date('Y'); ?> <a href="http://ndfnet.com/">NDFNet</a></address>
-        </center>
+    <?php
+        if (isset($_SERVER['HTTPS'])) {
+            echo '<a href="https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] . '">Switch to HTTPS</a>';
+        } else {
+            echo '<a href="http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] . '">Switch to HTTP</a>';
+        } 
+    ?>
+    <address>&copy; <?=date('Y')?> <a href="http://ndfnet.com/">NDFNet</a></address>
+</center>
