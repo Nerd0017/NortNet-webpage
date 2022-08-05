9c98777fde951c0afd647a4d3f2746a3428c9083
Frankie B <floppydisk05@aol.com>
2022-08-05 19:57:51 +0000

    Improve page formatting

--- bookmarks/index.php
+++ bookmarks/index.php
@@ -1,7 +1,6 @@
 <html>
 <head>
     <?php require('../head.php'); ?>
-
     <!--<meta property="og:type" content="website">
     <meta property="og:title" content="nick and floppy's Bookmarks">
     <meta property="og:description" content="Websites we think are cool">
@@ -12,55 +11,55 @@
 
 <body>
 	<center><table border="1" bgcolor="3c3c84" width="800px"><tr><td>
-    	<?php require('../pageheader.html'); ?>
-    	<hr>
-    	<h1>Bookmarks <small>(cool websites)</small></h1>
-        <hr>
-        <br>
-        <h2>Computing &amp; Technology</h2>
-        <hr>
-        <table><td><img src="./icons/winworld.png" title="WinWorldPC icon"></td><td><h2><a href="https://winworldpc.com/home">WinWorldPC</a></h2></td></table>
-        <p>WinWorld is an online museum dedicated to the preservation and sharing of vintage, abandoned, and pre-release software.</p>
-        <br>
-        <table><td><img src="./icons/escargot.png" title="Escargot icon"></td><td><h2><a href="http://escargot.chat/">Escargot</a></h2></td></table>
-        <p>MSN & Yahoo Messenger Revival</p>
-        <br>
-        <table><td><img src="./icons/nina.png" title="NINA icon"></td><td><h2><a href="https://nina.chat/">NINA</a></h2></td></table>
-        <p>Upcoming revival of the America Online service.</p>
-        <br>
-        <h2>Friends' Sites</h2>
-        <hr>
-        <table><td><img src="./icons/noicon.png" title="No icon available"></td><td><h2><a href="https://joshuaalto.com/">Sashi</a></h2></td></table>
-        <p>...lol marquees</p>
-        <br>
-        <table><td><img src="./icons/danielle.png" title="Danielle Wheeler icon"></td><td><h2><a href="https://danika.jukor.net/">Danielle Wheeler</a></h2></td></table>
-        <p>Supporting Vintage and Modern Windows, from 3.1x to 10</p>
-        <br>
-        <table><td><img src="./icons/strangeness.png" width="32" height="32" title="Strangeness Blog icon"></td><td><h2><a href="http://strangenessblog.net/">Strangeness Blog</a></h2></td></table>
-        <p>noone's blog</p>
-        <br>
-        <table><td><img src="./icons/noicon.png" title="No icon available"></td><td><h2><a href="https://campos02.neocities.org/">campos</a></h2></td></table>
-        <p>Cool brazilian dude, does programming and stuff</p>
-        <br>
-        <table><td><img src="./icons/hiden.png" title="HIDEN64 icon"></td><td><h2><a href="https://hiden.pw/">HIDEN64</a></h2></td></table>
-        <p>Moar buttons!</p>
-        <br>
-        <table><td><img src="./icons/noicon.png" title="No icon available"></td><td><h2><a href="http://cocomark.neocities.org/">Coco's Website</a></h2></td></table>
-        <p>Microsoft Edge 89.0.774.75</p>
-        <br>
-        <h2>Other Cool Stuff</h2>
-        <hr>
-        <table><td><img src="./icons/noicon.png" title="No icon available"></td><td><h2><a href="http://toastytech.com/">Toastytech.com</a></h2></td></table>
-        <p>Nathan's Toasty Technology Page</p>
-        <br>
-        <table><td><img src="./icons/noicon.png" title="No icon available"></td><td><h2><a href="http://www.hoary.org/browse/">Optimized for no one</a></h2></td></table>
-        <p>Optimized for no one, but pretty much OK with . . .</p>
-        <br>
-        <table><td><img src="./icons/cameronsworld.png" title="Cameron's World icon"></td><td><h2><a href="https://www.cameronsworld.net/">Cameron's World</a></h2></td></table>
-        <p>A love letter to the Internet of old.</p>
-        <br>
-        <hr>
-		<?php require('../footer.php'); ?>
+    <?php require('../pageheader.html'); ?>
+    <hr>
+    <h1>Bookmarks <small>(cool websites)</small></h1>
+    <hr>
+    <br>
+    <h2>Computing &amp; Technology</h2>
+    <hr>
+    <table><td><img src="./icons/winworld.png" title="WinWorldPC icon"></td><td><h2><a href="https://winworldpc.com/home">WinWorldPC</a></h2></td></table>
+    <p>WinWorld is an online museum dedicated to the preservation and sharing of vintage, abandoned, and pre-release software.</p>
+    <br>
+    <table><td><img src="./icons/escargot.png" title="Escargot icon"></td><td><h2><a href="http://escargot.chat/">Escargot</a></h2></td></table>
+    <p>MSN & Yahoo Messenger Revival</p>
+    <br>
+    <table><td><img src="./icons/nina.png" title="NINA icon"></td><td><h2><a href="https://nina.chat/">NINA</a></h2></td></table>
+    <p>Upcoming revival of the America Online service.</p>
+    <br>
+    <h2>Friends' Sites</h2>
+    <hr>
+    <table><td><img src="./icons/noicon.png" title="No icon available"></td><td><h2><a href="https://joshuaalto.com/">Sashi</a></h2></td></table>
+    <p>...lol marquees</p>
+    <br>
+    <table><td><img src="./icons/danielle.png" title="Danielle Wheeler icon"></td><td><h2><a href="https://danika.jukor.net/">Danielle Wheeler</a></h2></td></table>
+    <p>Supporting Vintage and Modern Windows, from 3.1x to 10</p>
+    <br>
+    <table><td><img src="./icons/strangeness.png" width="32" height="32" title="Strangeness Blog icon"></td><td><h2><a href="http://strangenessblog.net/">Strangeness Blog</a></h2></td></table>
+    <p>noone's blog</p>
+    <br>
+    <table><td><img src="./icons/noicon.png" title="No icon available"></td><td><h2><a href="https://campos02.neocities.org/">campos</a></h2></td></table>
+    <p>Cool brazilian dude, does programming and stuff</p>
+    <br>
+    <table><td><img src="./icons/hiden.png" title="HIDEN64 icon"></td><td><h2><a href="https://hiden.pw/">HIDEN64</a></h2></td></table>
+    <p>Moar buttons!</p>
+    <br>
+    <table><td><img src="./icons/noicon.png" title="No icon available"></td><td><h2><a href="http://cocomark.neocities.org/">Coco's Website</a></h2></td></table>
+    <p>Microsoft Edge 89.0.774.75</p>
+    <br>
+    <h2>Other Cool Stuff</h2>
+    <hr>
+    <table><td><img src="./icons/noicon.png" title="No icon available"></td><td><h2><a href="http://toastytech.com/">Toastytech.com</a></h2></td></table>
+    <p>Nathan's Toasty Technology Page</p>
+    <br>
+    <table><td><img src="./icons/noicon.png" title="No icon available"></td><td><h2><a href="http://www.hoary.org/browse/">Optimized for no one</a></h2></td></table>
+    <p>Optimized for no one, but pretty much OK with . . .</p>
+    <br>
+    <table><td><img src="./icons/cameronsworld.png" title="Cameron's World icon"></td><td><h2><a href="https://www.cameronsworld.net/">Cameron's World</a></h2></td></table>
+    <p>A love letter to the Internet of old.</p>
+    <br>
+    <hr>
+	<?php require('../footer.php'); ?>
 	</td></tr></table></center>
 </body>
 </html>
--- footer.php
+++ footer.php
@@ -1,10 +1,10 @@
-<center>
-    <?php
-        if (isset($_SERVER['HTTPS'])) {
-            echo '<a href="https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] . '">Switch to HTTPS</a>';
-        } else {
-            echo '<a href="http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] . '">Switch to HTTP</a>';
-        } 
-    ?>
-    <address>&copy; <?=date('Y')?> <a href="http://ndfnet.com/">NDFNet</a></address>
-</center>
+<!-- BEGIN PAGE FOOTER -->
+    <center>
+        <?php
+        if (isset($_SERVER['HTTPS'])) { echo '<a href="https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] . '">Switch to HTTPS</a>'; }
+        else { echo '<a href="http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] . '">Switch to HTTP</a>'; } 
+        ?>
+
+        <address>&copy; <?=date('Y')?> <a href="http://ndfnet.com/">NDFNet</a></address>
+    </center>
+    <!-- END PAGE FOOTER -->
